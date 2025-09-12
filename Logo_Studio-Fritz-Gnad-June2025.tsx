c2273a172232653ae794f7c0aa7e28e31cb9347a
Fritz Gnad <fritzgnad@25fd0e2a-87b9-47e2-b507-6bad6ec1c24e.fritz.box>
2025-09-12 09:10:17 +0200

    chore: add project files (initial import)

---
+++ Logo_Studio-Fritz-Gnad-June2025.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Logo_Studio-Fritz-Gnad-June2025" tilewidth="64" tileheight="64" tilecount="888" columns="37">
+ <image source="tilesets/Logo_Studio-Fritz-Gnad-June2025.png" width="2405" height="1565"/>
+</tileset>
