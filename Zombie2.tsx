4f836bad3871577cadf35befea306402b70a200e
Fritz Gnad <fritzgnad@FritzsMacStudio.fritz.box>
2025-09-26 09:35:19 +0200

    added jitsi properties, new floor material

---
+++ Zombie2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Zombie2" tilewidth="32" tileheight="32" tilecount="800" columns="20">
+ <image source="tilesets/craftpix-551899-free-zombie-tds-tilesets-buildings-and-furniture/Tilelists/Tilelist2.png" width="652" height="1302"/>
+</tileset>
---
+++ Zombie_Buildings_furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Zombie_Buildings_furniture" tilewidth="32" tileheight="32" tilecount="560" columns="20">
+ <image source="tilesets/craftpix-551899-free-zombie-tds-tilesets-buildings-and-furniture/Tilelists/Tilelist1.png" width="652" height="912"/>
+</tileset>
